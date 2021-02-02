a9d0a33af10c31f5aa76c4ddf8a496ac3336e419
Japhia Chen <qiuzhuo196@gmail.com>
2021-02-02 23:26:51 +0800

    path planing using cluster result

--- information_input.py
+++ information_input.py
@@ -5,15 +5,15 @@ from class_definition import Target
 from class_definition import No_fly_zone
 
 
-def solve_info(in_stream):
+def solve_info(in_stream1,in_stream2):
     uavs = {}
     no_fly_zones = {}
 
     # 此行是UAV信息项介绍
-    _ = in_stream.readline().strip()
-    N = int(in_stream.readline())       # UAV总数
+    _ = in_stream1.readline().strip()
+    N = int(in_stream1.readline())       # UAV总数
     for _ in range(N):      # 循环从txt中读取信息，生成UAV类的多个实例
-        items = in_stream.readline().strip().split(',')     # 每行先读到items变量
+        items = in_stream1.readline().strip().split(',')     # 每行先读到items变量
         id = items[0]   # 首位是id
         position = float(items[1]), float(items[2])     # 第二、三位是position
         direction = float(items[3]), float(items[4])  # 第四、五位是direction
@@ -21,12 +21,12 @@ def solve_info(in_stream):
         uavs[id] = UAV(id, position, direction, [], cost, 0)
 
     # 此行是Target信息项介绍
-    _ = in_stream.readline().strip()
+    _ = in_stream2.readline().strip()
     for id in uavs:
         targets = {}
-        M = int(in_stream.readline())       # Target总数
+        M = int(in_stream2.readline()[0])       # Target总数
         for _ in range(M):      # 循环从txt中读取信息，生成Target类的多个实例
-            items = in_stream.readline().strip().split(',')     # 每行先读到items变量
+            items = in_stream2.readline().strip().split(',')     # 每行先读到items变量
             targets_id = items[0]   # 首位是id
             position = float(items[1]), float(items[2])     # 第二、三位是position
             reward = items[3]     # 第四位是reward
@@ -34,11 +34,11 @@ def solve_info(in_stream):
             heappush(uavs[str(id)].task_list, targets[str(targets_id)].position)
 
     # 此行是禁飞区信息项介绍
-    _ = in_stream.readline().strip()
-    L = int(in_stream.readline())       # 禁飞区总数
+    _ = in_stream1.readline().strip()
+    L = int(in_stream1.readline())       # 禁飞区总数
     for _ in range(L):      # 循环从txt中读取信息，生成Target类的多个实例
-        item1 = in_stream.readline().strip().split(',')     # 每行先读到items变量
-        item2 = in_stream.readline().strip().split(',')
+        item1 = in_stream1.readline().strip().split(',')     # 每行先读到items变量
+        item2 = in_stream1.readline().strip().split(',')
         id = item1[0]
         zone_x = list(map(float,item1[1:]))     # 第二、三行是禁飞区
         zone_y = list(map(float,item2[1:]))
--- main.py
+++ main.py
@@ -17,13 +17,14 @@ class Test(unittest.TestCase):     # testcase
     def test_solve(self):
 
         rootdir = os.path.dirname(__file__)
-        with open(os.path.join(rootdir, 'UAVs_Targets_info.in.txt')) as inputFile:  # 打开输入信息txt
-            uavs, no_fly_zones = solve_info(inputFile)
-            init_draw(uavs, no_fly_zones)  # 画出原始地图
-            for id in uavs:
-                path = astar(uavs[str(id)], uavs[str(id)].task_list, no_fly_zones)  # 调用astar生成可行的路径
-                draw_one(path)
-            plt.show()
+        uavs_info = open(os.path.join(rootdir, 'UAVs_info.in.txt'),mode='r')
+        tagets_info = open(os.path.join(rootdir, 'Targets_info.in.txt'),mode='r')
+        uavs, no_fly_zones = solve_info(uavs_info,tagets_info)
+        init_draw(uavs, no_fly_zones)  # 画出原始地图
+        for id in uavs:
+            path = astar(uavs[str(id)], uavs[str(id)].task_list, no_fly_zones)  # 调用astar生成可行的路径
+            draw_one(path)
+        plt.show()
 
 
 if __name__ == '__main__':
